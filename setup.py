3cbcf6ebdee095fc09275b57fb1fc29030004529
shree <shreyashmohod@gmail.com>
2022-12-07 11:51:30 +0530

    3

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages,setup
-from typing import list
+from typing import List
 
 REQUIREMENT_FILE_NAME = "requirements.txt"
 HYPHEN_E_DOT = "-e ."
@@ -8,7 +8,7 @@ def get_requirements()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
-    
+
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
@@ -17,8 +17,8 @@ def get_requirements()->List[str]:
 setup(
     name="sensor",
     version="0.0.1",
-    author="ineuron",
-    author_email="avnish@ineuron.ai",
+    author="shree",
+    author_email="shreyashmohod@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
