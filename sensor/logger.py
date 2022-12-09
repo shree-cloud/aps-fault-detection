3bf665240524fdb12fc56dc548691248575217c2
shree <shreyashmohod@gmail.com>
2022-12-09 19:02:53 +0530

    5

--- sensor/logger.py
+++ sensor/logger.py
@@ -5,7 +5,7 @@ from datetime import datetime
 LOG_FILE_NAME = f"{datetime().now().strftime('%m%d%Y__%H%M_%S')}.log"
 
 LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
-os.makedirs(LOG_FILE_DIR,exist_ok=True).  #if the folder is available
+os.makedirs(LOG_FILE_DIR,exist_ok=True)  #if the folder is available
 
 #path of log file
 LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
