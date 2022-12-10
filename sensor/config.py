85f248bf22b5266aeab8234a64d2961b1d9d5347
shree <shreyashmohod@gmail.com>
2022-12-10 15:05:04 +0530

    7

--- sensor/config.py
+++ sensor/config.py
@@ -11,4 +11,4 @@ class EnvironmentVariable:
     mongo_db_url:str = os.getenv("MONGO_DB_URL")
 
 env_var = EnvironmentVariable()
-client = pymongo.MongoClient()
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
