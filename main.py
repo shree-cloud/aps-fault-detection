9b5c1a4fe6d54d1b9a701e829d5081ce2417d4e0
shree <shreyashmohod@gmail.com>
2023-01-03 10:13:43 +0530

    jf

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ file_path = "/config/workspace/aps_failure_training_set1.csv"
 if __name__=="__main__":
      try:
           # start_training_pipeline()
+          # 1111
           output_file = start_batch_prediction(input_file_path=file_path)
           print(output_file)
      except Exception as e:
